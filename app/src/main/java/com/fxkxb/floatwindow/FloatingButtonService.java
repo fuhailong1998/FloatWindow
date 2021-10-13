3a1cf0c9aea8df2087229002d86016a76ef598e8
hailong.fu <hailong.fu@thundersoft.com>
2021-10-13 18:22:54 +0800

    Oct 13 18:22

--- app/src/main/java/com/fxkxb/floatwindow/FloatingButtonService.java
+++ app/src/main/java/com/fxkxb/floatwindow/FloatingButtonService.java
@@ -1,6 +1,7 @@
 package com.fxkxb.floatwindow;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
@@ -11,14 +12,18 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 
 /**
- * @author : Hailong Fu (hailong.fu@thundersoft.com)
+ * @author : Leon(fxkxb.com)
  * @version : 1.0
  * @file : FloatingService.class
  * @date : October 12,2021 15:29
@@ -27,35 +32,45 @@ import androidx.annotation.Nullable;
 
 public class FloatingButtonService extends Service {
     public static boolean isStarted = false;
-
     private WindowManager windowManager;
     private WindowManager.LayoutParams layoutParams;
-
     private Button button;
 
     @Override
     public void onCreate() {
-
         super.onCreate();
 
-        Log.e("TAG", "onCreate: ");
 
         isStarted = true;
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
         layoutParams = new WindowManager.LayoutParams();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
         } else {
+//            对不同版本的Android系统进行适配
             layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
         }
+
+
+// 设置图片格式，效果为背景透明
         layoutParams.format = PixelFormat.RGBA_8888;
+
+//      gravity不是说你添加到WindowManager中的View相对屏幕的几种放置,而是说你可以设置参考系
+//        例如:mWinParams.gravity= Gravity.LEFT | Gravity.TOP;意思是以屏幕左上角为参考系,那么屏幕左上角的坐标就是(0,0),这是你后面摆放View位置的唯一依据.
+//        当你设置为mWinParams.gravity = Gravity.CENTER;那么你的屏幕中心为参考系,坐标(0,0).一般我们用屏幕左上角为参考系.
         layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        layoutParams.width = 500;
-        layoutParams.height = 200;
-        layoutParams.x = 300;
-        layoutParams.y = 300;
+        layoutParams.alpha = 0.5F;
+        // 设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_SECURE;
+        layoutParams.width = 400;
+        layoutParams.height = 400;
+        layoutParams.x = 0;
+        layoutParams.y = 0;
+
     }
+
 
     @Nullable
     @Override
@@ -72,11 +87,12 @@ public class FloatingButtonService extends Service {
     private void showFloatingWindow() {
         if (Settings.canDrawOverlays(this)) {
             button = new Button(getApplicationContext());
-            button.setText("Floating Window");
-            button.setBackgroundColor(Color.BLUE);
+//            button.setText("Floating Window");
+            button.setBackgroundColor(Color.MAGENTA);
             windowManager.addView(button, layoutParams);
-
             button.setOnTouchListener((View.OnTouchListener) new FloatingOnTouchListener());
+//            button.setOnClickListener(v -> Toast.makeText(getApplicationContext(),"You Click the Float Window", Toast.LENGTH_SHORT).show());
+
         }
     }
 
@@ -90,6 +106,10 @@ public class FloatingButtonService extends Service {
                 case MotionEvent.ACTION_DOWN:
                     x = (int) event.getRawX();
                     y = (int) event.getRawY();
+
+
+                    Toast.makeText(getApplicationContext(),
+                            "Location: x = " + x + " , y = " + y, Toast.LENGTH_SHORT).show();
                     break;
                 case MotionEvent.ACTION_MOVE:
                     int nowX = (int) event.getRawX();
@@ -101,6 +121,7 @@ public class FloatingButtonService extends Service {
                     layoutParams.x = layoutParams.x + movedX;
                     layoutParams.y = layoutParams.y + movedY;
                     windowManager.updateViewLayout(view, layoutParams);
+                    Log.e("Locaiton", "onTouch: X is :" + x + " Y is :" + y);
                     break;
                 default:
                     break;
--- app/src/main/java/com/fxkxb/floatwindow/MainActivity.java
+++ app/src/main/java/com/fxkxb/floatwindow/MainActivity.java
@@ -1,5 +1,9 @@
 package com.fxkxb.floatwindow;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    ActivityResultLauncher<Intent> intentActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+
+        }
+    });
 
     public void startFloatingButtonService(View view) {
         if (FloatingButtonService.isStarted) {
@@ -36,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
         }
         if (!Settings.canDrawOverlays(this)) {
             Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
-            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0);
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+//            startActivityForResult(intent, 0);已经过时解决方案
+            intentActivityResultLauncher.launch(intent);
         } else {
-            Log.e("TAG", "startFloatingButtonService: ");
+//            开启服务守护悬浮窗
             startService(new Intent(MainActivity.this, FloatingButtonService.class));
         }
     }
